4657e48263f8be1fe88792d6c5914ddf8bb79463
Max Lyszczyk <mwlyszczyk@gmail.com>
2026-03-10 19:11:23 -0400

    finished implementing react router, pushup detection works extreamly poorly

--- src/pages/ExerciseSelect.jsx
+++ src/pages/ExerciseSelect.jsx
@@ -3,25 +3,36 @@ import "./ExerciseSelect.css";
 const EXERCISES = ["Push-ups", "Squats", "Jumping Jacks"];
 
 
-export default function ExerciseSelect({ onPickExercise, onBack }) {
+export default function ExerciseSelect() {
   return (
     <div>
       <h2 style={{ color: "white" }}>Choose an Exercise</h2>
 
       <div style={{ display: "grid", gap: "10px", maxWidth: "300px", margin: "0 auto" }}>
-              {EXERCISES.map((ex) => (
-             <Link to="/Pushup" className="link">
-          <button key={ex}>
-            {ex}
-          </button>
+              
+          <Link to="/Pushup" className="link">
+             <button>
+              Pushups
+            </button>
           </Link>
-        ))}
+          <Link to="/Squat">
+              <button>
+              Squats
+              </button>
+              </Link>
+              <Link to="/JumpingJack">
+                  <button>
+                  Jumping Jacks
+                  </button>
+              </Link>
+
+        
       </div>
 
       <div style={{ marginTop: "15px" }}>
               <Link to="/" className="link">
                 <button>
-                      Select
+                      Home
                 </button>
               </Link>
       </div>
